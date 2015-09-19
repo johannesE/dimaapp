b37fe73c71e9d346b9f3f86c21ecd7cb85eaa5cb
johannesE <johannes.eifert@unifr.ch>
2015-09-20 01:20:58 +0200

    route for the voting (testing automatic deployment)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :topics do
     resources :possibilities
     get :vote, to: 'possibilities#vote'
+    post :do_vote, to: 'possibilities#do_vote'
   end
   root to: 'visitors#index'
   devise_for :users
